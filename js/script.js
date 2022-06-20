9720e291d17f3c2527b77e3679e8d68e72fe6bfd
Tejas Naik <RNTejas2005@gmail.com>
2022-06-20 10:14:51 +0530

    Make navbar sticky

--- js/script.js
+++ js/script.js
@@ -1,4 +1,77 @@
-console.log("Hello, World!")
+///////////////////////////////////////////
+// Current year
+const year = document.querySelector('.year');
+const curretnYear = new Date().getFullYear()
+year.textContent = curretnYear
+
+// Make mobile navigation work
+const headerEl = document.querySelector('.header');
+const mobileBtnEl = document.querySelector('.btn-mobile-nav');
+mobileBtnEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
+});
+
+////////////////////////////////////////////
+// Smooth Scrolling 
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+
+///////////////////////////////////////////////////////////
+// Making the navigation sticky
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add('sticky')
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+
+  }
+);
+obs.observe(sectionHeroEl);
+
+
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
@@ -20,17 +93,6 @@ function checkFlexGap() {
 checkFlexGap();
 
 
-// Current year
-const year = document.querySelector('.year');
-const curretnYear = new Date().getFullYear()
-year.textContent = curretnYear
-
-// Make mobile navigation work
-const headerEl = document.querySelector('.header');
-const mobileBtnEl = document.querySelector('.btn-mobile-nav');
-mobileBtnEl.addEventListener('click', function () {
-  headerEl.classList.toggle('nav-open');
-});
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
